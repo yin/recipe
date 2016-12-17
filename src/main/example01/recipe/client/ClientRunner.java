6b086630460f79937a674dad54f7a4e21f33efb6
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2016-12-17 05:30:33 +0100

    Added jackson object mapping for Gauava immutable collections

--- src/main/example01/recipe/client/ClientRunner.java
+++ src/main/example01/recipe/client/ClientRunner.java
@@ -1,9 +1,14 @@
 package recipe.client;
 
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Provides;
+import com.google.inject.multibindings.Multibinder;
 import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
 import com.googlecode.jsonrpc4j.ProxyUtil;
 import org.slf4j.Logger;
@@ -18,6 +23,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collection;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -31,6 +37,7 @@ public class ClientRunner implements Runnable {
     private final URL url;
 
     public static void main(String[] args) throws IOException {
+        try{new ClientRunner(parseArgs(args)).run();}catch (Exception e) {e.printStackTrace();}
         ExecutorService executor = Executors.newSingleThreadExecutor();
         executor.execute(new ClientRunner(parseArgs(args)));
     }
@@ -69,15 +76,28 @@ public class ClientRunner implements Runnable {
     class ClientModule extends AbstractModule {
         @Override
         protected void configure() {
+            jacksonConfigure();
         }
 
         @Provides
-        public ItemQueryService createItemQueryClient() {
-            ItemQueryService proxy;
-            JsonRpcHttpClient client;
-            client = new JsonRpcHttpClient(url);
-            proxy = ProxyUtil.createClientProxy(getClass().getClassLoader(), ItemQueryService.class, client);
+        public JsonRpcHttpClient createJsonRpcHttpClient(ObjectMapper mapper) {
+            return new JsonRpcHttpClient(mapper, url, ImmutableMap.of());
+        }
+
+        @Provides
+        public ItemQueryService createItemQueryClient(JsonRpcHttpClient client) {
+            ItemQueryService proxy =
+                    ProxyUtil.createClientProxy(getClass().getClassLoader(), ItemQueryService.class, client);
             return proxy;
+        }
+
+        private void jacksonConfigure() {
+            Multibinder multi = Multibinder.newSetBinder(binder(), Module.class);
+            multi.addBinding().to(GuavaModule.class);
+        }
+
+        ObjectMapper createObjectMapper(Set<Module> jascksonModules) {
+            return new ObjectMapper().registerModules(jascksonModules);
         }
     }
 }
