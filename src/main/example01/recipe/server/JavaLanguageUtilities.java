44373cdb8eb98075e9fe5c4a52775e5e9b04c460
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2016-12-17 09:47:14 +0100

    test for JavaLanguageUtilities

--- src/main/example01/recipe/server/JavaLanguageUtilities.java
+++ src/main/example01/recipe/server/JavaLanguageUtilities.java
@@ -3,12 +3,16 @@ package recipe.server;
 import java.lang.reflect.Array;
 import java.util.Collection;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  * Performs different operations (currently a signle one) reated to java lanague construct like conversions
  * between data structures.
  */
 public class JavaLanguageUtilities {
     public static <T> T[] constructArray(Class<T> type, Collection<T> head, T... tail) {
+        checkNotNull(type);
+        checkNotNull(head);
         T[] ary = (T[]) Array.newInstance(type, head.size() + tail.length);
         int index = 0;
         for (T element : head) {
---
+++ src/test/java/recipe/server/JavaLanguageUtilitiesTest.java
@@ -0,0 +1,66 @@
+package recipe.server;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static recipe.server.JavaLanguageUtilitiesTest.TestOs.*;
+
+public class JavaLanguageUtilitiesTest {
+    // 'TestOs' - that's a name for an abstraction!
+    enum TestOs {
+        O0, O1, O2
+    }
+
+    @Test
+    public void constructArray_head_null() throws Exception {
+        try {
+            TestOs[] ary = JavaLanguageUtilities.constructArray(TestOs.class, null);
+            fail("should check for null lists");
+        } catch (IllegalArgumentException e) {
+            // success
+        }
+    }
+
+    @Test
+    public void constructArray_head_null_tail_single() throws Exception {
+        try {
+            TestOs[] ary = JavaLanguageUtilities.constructArray(TestOs.class, null, O0);
+            fail("should check for null lists");
+        } catch (IllegalArgumentException e) {
+            // success
+        }
+    }
+
+    @Test
+    public void constructArray_head_empty() throws Exception {
+        TestOs[] ary = JavaLanguageUtilities.constructArray(TestOs.class, ImmutableList.of());
+        assertEquals(0, ary.length);
+    }
+
+    @Test
+    public void constructArray_head_empty_tail_single() throws Exception {
+        TestOs[] ary = JavaLanguageUtilities.constructArray(TestOs.class, ImmutableList.of(), O0);
+        assertArrayEquals(new TestOs[]{O0}, ary);
+    }
+
+    @Test
+    public void constructArray_head_single() throws Exception {
+        TestOs[] ary = JavaLanguageUtilities.constructArray(TestOs.class, ImmutableList.of());
+        assertEquals(0, ary.length);
+    }
+
+    @Test
+    public void constructArray_head_single_tail_single() throws Exception {
+        TestOs[] ary = JavaLanguageUtilities.constructArray(TestOs.class, ImmutableList.of(), O0);
+        assertArrayEquals(new TestOs[]{O0}, ary);
+    }
+
+    @Test
+    public void constructArray_head_multiple_tail_multiple_overlapping() throws Exception {
+        TestOs[] ary = JavaLanguageUtilities.constructArray(TestOs.class, ImmutableList.of(O0, O1), O1, O2);
+        assertArrayEquals(new TestOs[]{O0, O1, O1, O2}, ary);
+    }
+}
