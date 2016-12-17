2ed2e4e5112f3798022de60ebf4911704eff5256
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2016-12-17 05:31:25 +0100

    logging for ItemQueryServiceimpl

--- src/main/example01/recipe/model/impl/ItemQueryServiceImpl.java
+++ src/main/example01/recipe/model/impl/ItemQueryServiceImpl.java
@@ -3,6 +3,8 @@ package recipe.model.impl;
 import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import recipe.model.messages.Item;
 import recipe.model.messages.QueryRequest;
 import recipe.model.messages.QueryResponse;
@@ -12,8 +14,11 @@ import recipe.model.services.ItemQueryService;
  * Serves queries by always returning the same set of Items, no matter what's the query.
  */
 public class ItemQueryServiceImpl implements ItemQueryService {
+    private static final Logger log = LoggerFactory.getLogger(ItemQueryServiceImpl.class);
+
     @Override
     public QueryResponse query(QueryRequest request) {
+        log.info("A query has been requested");
         return new QueryResponse() {
             @Override
             public ImmutableCollection<Item> results() {
