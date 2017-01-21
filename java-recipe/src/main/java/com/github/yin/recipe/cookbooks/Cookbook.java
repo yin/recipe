720249a420bd60bd80f51d731170589b16d36dbd
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2017-01-21 20:41:47 +0100

    [wip] Cookbook services

--- java-recipe/src/main/java/com/github/yin/recipe/cookbooks/Cookbook.java
+++ java-recipe/src/main/java/com/github/yin/recipe/cookbooks/Cookbook.java
@@ -3,6 +3,7 @@ package com.github.yin.recipe.cookbooks;
 import com.github.yin.recipe.model.Ingredient;
 import com.github.yin.recipe.templating.TargetTemplate;
 
+import java.io.OutputStream;
 import java.util.ServiceLoader;
 
 /**
@@ -12,9 +13,24 @@ import java.util.ServiceLoader;
  */
 public interface Cookbook<T extends Ingredient> {
     Class<T> ingredientClass();
-    IngredientProcessor processor(Ingredient ingredient);
-    OutStreamLocator outStreamLocatorFor(Ingredient ingredient);
-    TargetMaterializer targetFor(Ingredient ingredient);
-
+    IngredientProcessor processor();
+    OutStreamLocator outStreamLocator();
+    TargetMaterializer materializer();
     TargetTemplate targetTemplate(String target);
+
+    /**
+     * Processes {@link Ingredient}'s and produces TargetElements to be materialized.
+     */
+    @FunctionalInterface
+    interface IngredientProcessor<T extends Ingredient> {
+        void process(T ingredient, String target) throws Exception;
+    }
+
+    /**
+     * Locates destinations of {@link TargetTemplate.TargetElement}'s
+     * and provides {@link java.io.OutputStream}'s for the {@link TargetMaterializer}.
+     */
+    interface OutStreamLocator {
+        OutputStream locate(TargetTemplate.TargetElement element);
+    }
 }
